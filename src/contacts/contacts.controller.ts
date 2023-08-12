78a699c0bc1cb0929a13341800c27a44c46bafe0
hitenSharm <hiten.sharm1708@gmail.com>
2023-08-12 15:31:21 +0530

    finished and tested

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -4,6 +4,7 @@ import { Contact } from './entities/contact.entity';
 import { Repository } from 'typeorm';
 import { ContactsService } from './contacts.service';
 import { RequestDTO } from './dtos/requestDTO.dto';
+import { ContactResponseDTO } from './dtos/contactResponse.dto';
 
 @Controller('identify')
 export class ContactsController {
@@ -13,9 +14,9 @@ export class ContactsController {
     ){}    
 
     @Post()
-    async findAll(@Body() request:RequestDTO): Promise<Contact[]> {        
+    async findAll(@Body() request:RequestDTO): Promise<ContactResponseDTO> {        
         let res=await this.contactService.identityFinder(request);
-        return null;
+        return res;
       }
 
 }
